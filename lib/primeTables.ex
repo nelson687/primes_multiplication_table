5f4557ac869fc8842066b78c41f050141329949c
Nelson Gallardo <gnelson85@gmail.com>
2017-08-24 20:17:15 +0100

    get list of primes algorithm

--- lib/primeTables.ex
+++ lib/primeTables.ex
@@ -6,4 +6,28 @@ defmodule PrimeTables do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def list_of_primes(n) do
+    primes = remove_multiples(Enum.to_list(2..n), n, :math.sqrt(n))
+    primes
+  end
+
+  # def remove_multiples(list, index_to_replace, i, n) when index_to_replace <= n do
+  #   remove_multiples(List.replace_at(list, index_to_replace, false), i+index_to_replace, i, n)
+  # end
+
+  def remove_multiples([head | tail], n, l) when head <= l do
+    [head | remove_multiples(Enum.filter(tail, fn e -> rem(e, head) !== 0 end), n, l)]
+  end
+  
+  def remove_multiples([head | tail], _, l) when head > l do
+    [head | tail]
+  end
+
+  def remove_multiples([], _, _) do
+    []
+  end
+
+
 end
+
