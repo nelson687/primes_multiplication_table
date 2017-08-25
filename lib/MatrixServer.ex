80abb97e3d9a96ef1c9ba9f97fbafd9784287c9e
Nelson Gallardo <gnelson85@gmail.com>
2017-08-25 21:02:09 +0100

    temp

--- lib/MatrixServer.ex
+++ lib/MatrixServer.ex
@@ -13,6 +13,10 @@ defmodule MatrixServer do
       GenServer.cast(pid, {:number, number})
   end
 
+  def put_new_entry(pid, number) do
+      GenServer.cast(pid, {:put_new_entry, number})
+  end
+
   def get_matrix(pid) do
       GenServer.call(pid, :get_matrix, @timeout)
   end
@@ -24,12 +28,33 @@ defmodule MatrixServer do
   end
 
   def handle_cast({:number, number}, matrix) do
-      new_matrix = 
-        matrix
-        |> Enum.reduce(matrix, fn({k,_v}, acc) -> put_in(acc[k][number], k * number) end)
-        |> Map.put(number, %{})
+    #  IO.puts(inspect(number))
+      # new_matrix = Map.put(matrix, number, %{})
+      
+      new_matrix_1 = Enum.reduce(matrix, matrix, fn({k,_v}, acc) -> put_in(acc[k][number], k * number) end)
+      # new_matrix_1 = Enum.reduce(matrix, matrix, fn({k,_v}, acc) -> put_in(acc[k][number], k * number) end)
+      # new_matrix = Enum.into(matrix, %{number => %{}})
 
-        new_matrix = Enum.reduce(new_matrix, new_matrix, fn({k,_v}, acc) -> put_in(acc[number][k], k * number) end)
+      # ne = Map.merge(new_matrix_1, new_matrix, fn _, a, b -> Map.merge(a, b) end)
+        # IO.puts(inspect(new_matrix_1))
+
+      # new_matrix2 = Enum.reduce(new_matrix_1, new_matrix_1, fn({k,_v}, acc) -> put_in(acc[number][k], k * number) end)
+      # IO.puts(inspect(new_matrix2))
+      {:noreply, new_matrix_1}
+  end
+
+  def handle_cast({:put_new_entry, number}, matrix) do
+    #  IO.puts(inspect(number))
+      new_matrix = Map.put(matrix, number, %{})
+      # new_matrix_1 = Enum.reduce(new_matrix, new_matrix, fn({k,_v}, acc) -> put_in(acc[k][number], k * number) end)
+      # new_matrix_1 = Enum.reduce(matrix, matrix, fn({k,_v}, acc) -> put_in(acc[k][number], k * number) end)
+      # new_matrix = Enum.into(matrix, %{number => %{}})
+
+      # ne = Map.merge(new_matrix_1, new_matrix, fn _, a, b -> Map.merge(a, b) end)
+        # IO.puts(inspect(length(new_matrix)))
+    #   new_matrix2 = Enum.reduce(new_matrix1, new_matrix1, fn({k,_v}, acc) -> put_in(acc[number][k], k * number) end)
+    #  IO.puts("incomplete matrix")  
+    # IO.puts(inspect(new_matrix))
       {:noreply, new_matrix}
   end
 
--- test/primeTables_test.exs
+++ test/primeTables_test.exs
@@ -6,11 +6,11 @@ defmodule PageControllerTest do
 #     assert prime_number == [2]
 #   end
 
-  test "Returns list of primes for input == 3" do
-    expected_list = [2, 3]
-    list_of_primes = PrimeTables.list_of_primes(350000)
-    assert list_of_primes == expected_list
-  end
+  # test "Returns list of primes for input == 3" do
+  #   expected_list = [2, 3]
+  #   list_of_primes = PrimeTables.list_of_primes(100000)
+  #   assert list_of_primes == expected_list
+  # end
 
 #   test "Returns list of primes for input == 5" do
 #     expected_list = [2, 3, 5]
@@ -23,4 +23,16 @@ defmodule PageControllerTest do
 #     list_without_multiples = PrimeTables.list_of_primes(10)
 #     assert list_without_multiples == expected_list
 #   end
+
+  test "Calculates matrix" do
+    list_of_primes = PrimeTables.list_of_primes(250000)
+    IO.inspect(length(list_of_primes))
+    # list_of_primes = PrimeTables.list_of_primes(5)
+    # IO.puts("NELSON")
+    # IO.inspect(list_of_primes)
+    res = PrimeTables.matrix(list_of_primes)
+    # IO.puts("matrix")
+    IO.inspect(res)
+    assert true
+  end
 end
